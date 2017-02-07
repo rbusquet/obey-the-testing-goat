3988af0a69345567a9bc76f6507cc729b05cab0f
Ricardo Busquet <ricardobusquet@gmail.com>
2017-02-06 22:50:53 -0800

    testing to-do list generation

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # The titles tell me what the site is: a Todo list site
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # You should be prompted to add a new todo list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a todo-item'
+        )
 
         # Type "buy milk".
-
+        inputbox.send_keys('buy milk')
 
         # This should create a todo list, I suppose.
         # You click enter, the page reloads and now lists "1. buy milk".
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.test == '1. buy milk' for row in rows)
+        )
 
         # You can type again. Type "make chocolate".
+        self.fail('Finish test')
 
         # Hit enter. Page loads. "2. make chocolate" now appears.
 
