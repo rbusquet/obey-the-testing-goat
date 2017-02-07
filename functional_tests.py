fbc4750720e5473d0e135f5007f2d146f431da39
Ricardo Busquet <ricardobusquet@gmail.com>
2017-02-06 23:31:15 -0800

    refactor item in table test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Open this new todo list web page
@@ -36,9 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. buy milk', [row.text for row in rows])
+        self.check_for_row_in_list_table('1. buy milk')
 
         # You can type again. Type "make chocolate".
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -47,11 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Hit enter. Page loads. "2. make chocolate" now appears.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. buy milk', [row.text for row in rows])
-        self.assertIn('2. make chocolate' , [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1. buy milk')
+        self.check_for_row_in_list_table('2. make chocolate')
         # You know what to do. You want to save this.
         # The page should show you a unique URL for your list.
         # Some blablabla is writen about this
