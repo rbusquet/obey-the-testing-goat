884d6b2ed498d3f45d160dc4bbdce6ddbd94e06f
Ricardo Busquet <ricardobusquet@gmail.com>
2017-02-12 18:16:21 -0800

    redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,10 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def test_only_saves_item_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
 
@@ -16,8 +20,24 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_post_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 # Create your views here.
+from lists.models import Item
+
 def home_page(request):
+
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'items': items,
     })
