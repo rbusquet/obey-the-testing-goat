891d4a001b88a4f53ee976f1d9424e4c5968f94d
Ricardo Busquet <ricardobusquet@gmail.com>
2017-02-12 22:07:28 -0800

    list model. tests on lists

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('List', default=None)
+
+
+class List(models.Model):
+    pass
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 # Create your views here.
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -13,5 +13,7 @@ def view_list(request):
     })
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'],
+        list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
